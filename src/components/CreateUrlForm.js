5ae89bfade493c611b6e0df00315b0d15d30b35f
ethanryan <ethanryan@gmail.com>
2018-01-04 10:52:01 -0500

    updated CreateUrlForm

--- src/components/CreateUrlForm.js
+++ src/components/CreateUrlForm.js
@@ -5,71 +5,56 @@ import { Form, Grid } from 'semantic-ui-react'
 class CreateUrlForm extends Component {
 
   constructor(props) {
-    console.log('**** props from CreateUrlForm:', props)
     super(props)
     this.state=({
       link: '',
     })
-  this.handleCreateUrlFormSubmit = this.handleCreateUrlFormSubmit.bind(this)
-} //end of constructor
-
-
-// componentWillReceiveProps(props) { //need this lifecycle method to edit text in textarea
-//   this.setState({
-//     story: props.story
-//   })
-// }
-
-handleUrlInputChange(event) {
-  const link = event.target.value
-  this.setState({
-    link: link,
-  })
-}
-
-handleCreateUrlFormSubmit(event) {
-  event.preventDefault()
-  const link = {
-    link: this.state.link,
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-  console.log('CreateUrlForm submitted: ', this.state)
-  this.props.handleSubmit( link )
-  this.setState({link: ''}) //this clears form onSubmit
-}
-
-
-render() {
-
-  return(
-
-    <div className="CreateUrlForm">
-      <Grid>
-        <Grid.Row centered>
-          <Form onSubmit={this.handleCreateUrlFormSubmit}>
-
-            {/* <Form.Field required>
-              <label>Last name</label>
-              <Input placeholder='Full name' />
-            </Form.Field> */}
-
-            <Form.Field
-              label="Add URL"
-              placeholder="url here"
-              autoFocus
-              control="input"
-              type="text"
-              // width={12}
-              //key="heroName"
-              onChange={this.handleUrlInputChange.bind(this)} />
-
-              <Form.Button content="Submit" type="submit" primary/>
-
-            </Form>
-          </Grid.Row>
-        </Grid>
-      </div>
-    )
+  
+  handleChange(event) {
+    this.setState({
+      link: event.target.value,
+    })
   }
-}
 
-export default CreateUrlForm
+  handleSubmit(event) {
+    event.preventDefault()
+    const link = {
+      link: this.state.link,
+    }
+    console.log('CreateUrlForm submitted: ', this.state)
+    this.props.handleSubmit( link )
+    this.setState({link: ''})
+  }
+
+  render() {
+
+    return(
+
+      <div className="CreateUrlForm">
+        <Grid>
+          <Grid.Row centered>
+            <Form onSubmit={this.handleSubmit}>
+
+              <Form.Field
+                label="Add URL"
+                placeholder="url here"
+                autoFocus
+                control="input"
+                type="text"
+                value={this.state.link}
+                onChange={this.handleChange} />
+
+                <Form.Button content="Submit" type="submit" primary/>
+
+              </Form>
+            </Grid.Row>
+          </Grid>
+        </div>
+      )
+    }
+  }
+
+  export default CreateUrlForm
